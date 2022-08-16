54240ed11170ff3c403dbd746da0c8eacca231ed
Gabriel Araujo <araujo.gca214@gmail.com>
2022-08-16 10:41:11 -0300

    edited exercise-1 and added exercise-2

--- algorithm-lab/a-list/exercise-1/exercise-1.c
+++ algorithm-lab/a-list/exercise-1/exercise-1.c
@@ -5,9 +5,9 @@ int main()
 {
     int a;
     int b;
-    printf("Insert first number: ");
+    printf("Enter first number: ");
     scanf("%d", &a);
-    printf("Insert second number: ");
+    printf("Enter second number: ");
     scanf("%d", &b);
     printf("%d + %d = %d\n", a, b, a + b);
     printf("%d - %d = %d\n", a, b, a - b);
---
+++ algorithm-lab/a-list/exercise-2/exercise-2.c
@@ -0,0 +1,23 @@
+// Make a program that calculates the average temperature of one week
+#include <stdio.h>
+
+int main()
+{
+    int temp_mon, temp_tue, temp_wed, temp_thu, temp_fri, temp_sat, temp_sun;
+    printf("Enter monday temperature: ");
+    scanf("%i", &temp_mon);
+    printf("Enter tuesday temperature: ");
+    scanf("%i", &temp_tue);
+    printf("Enter wednesday temperature: ");
+    scanf("%i", &temp_wed);
+    printf("Enter thursday temperature: ");
+    scanf("%i", &temp_thu);
+    printf("Enter friday temperature: ");
+    scanf("%i", &temp_fri);
+    printf("Enter saturday temperature: ");
+    scanf("%i", &temp_sat);
+    printf("Enter sunday temperature: ");
+    scanf("%i", &temp_sun);
+    int average = (temp_mon + temp_tue + temp_wed + temp_thu + temp_fri + temp_sat + temp_sun) / 7;
+    printf("The average temperature of the week is: %i\n", average);
+}
