556fd691772a37df98618d7e00e5188e985ca583
Gabriel Araujo <araujo.gca214@gmail.com>
2022-08-16 15:28:12 -0300

    added exercise 4

---
+++ a-list/exercise-4.c
@@ -0,0 +1,11 @@
+// Write a program that reads the amount of rain in mm and prints this amount in inches
+#include <stdio.h>
+
+int main()
+{
+    float rain_mm;
+    printf("Enter amount of rain in mm: ");
+    scanf("%f", &rain_mm);
+    float rain_inches = rain_mm / 25.4;
+    printf("The amount of rain in inches is: %.2f\n", rain_inches);
+}
