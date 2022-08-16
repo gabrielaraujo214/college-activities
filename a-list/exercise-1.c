8dd75d04bc6c386610889d717ed6e0fbdd431d0b
Gabriel Araujo <araujo.gca214@gmail.com>
2022-08-16 15:29:56 -0300

    added exercise 4

---
+++ a-list/exercise-1.c
@@ -0,0 +1,17 @@
+// Write a program that reads two numbers and prints the result of adding, subtracting, multiplying and dividing these numbers.
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    printf("%d + %d = %d\n", a, b, a + b);
+    printf("%d - %d = %d\n", a, b, a - b);
+    printf("%d * %d = %d\n", a, b, a * b);
+    printf("%d / %d = %d\n", a, b, a / b);
+    return 0;
+}
